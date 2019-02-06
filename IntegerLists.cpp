45eb707f1cd5f6c914cabeb69c9f3ab8af851d4f
Jerry <jerryzhang28eclipse@gmail.com>
2019-02-06 21:49:55 +0800

    [Feat] Some implementation

--- IntegerLists.cpp
+++ IntegerLists.cpp
@@ -2,10 +2,27 @@
 #include <vector>
 #include <cmath>
 #include <string>
+#include <string.h>
+#include <algorithm>
+#include <list>
+#include <deque>
 
 using namespace std;
 
 int main() {
-
+    int cases;
+    cin >> cases;
+    while (cases--) {
+        string commands;
+        getline(cin, commands);
+        int listlen;
+        list<int> numbers;
+        cin >> listlen;
+        for (int i = 0; i < listlen; i++) {
+            int input;
+            cin >> input;
+            numbers.push_back(input);
+        }
+    }
     return 0;
 }
