d38843940c5b256369d00ddae96584778d756324
Lucria <jerryzhang28eclipse@gmail.com>
2019-02-08 21:08:25 +0800

    [Feat] Completed IntegerLists

--- DRMMessages.cpp
+++ DRMMessages.cpp
@@ -9,21 +9,25 @@ using namespace std;
 
 int main() {
     string message;
-    getline(cin, message);
+    cin >> message;
     int halflength = message.size() / 2;
     vector <int> half1, half2;
-    int sum1, sum2;
-    for (int i = 0; i < halflength; i++) {
-        int input1, input2;
+    int sum1 = 0;
+    int sum2 = 0;
+    int input1, input2;
+    for(int i = 0; i < halflength; i++) {
         input1 = message[i] - 'A';
-        input2 = message[i + halflength] - 'A';
         sum1 += input1;
-        sum2 += input2;
         half1.push_back(input1);
+    }
+    for(int i = halflength; i < message.size(); i++) {
+        input2 = message[i] - 'A';
+        sum2 += input2;
         half2.push_back(input2);
     }
+    
     for (int i = 0; i < halflength; i++) {
-        cout << char(((half1[i] + half2[i] + sum1 + sum2 - 1) % 26) + 'A');
+        cout << char(((half1[i] + half2[i] + sum1 + sum2) % 26) + 'A');
     }
     cout << endl;
     return 0;
--- IntegerLists.cpp
+++ IntegerLists.cpp
@@ -12,16 +12,62 @@ using namespace std;
 int main() {
     int cases;
     cin >> cases;
-    while (cases--) {
+    for(int i = 0; i < cases; i++) {
+        bool breakflag = false;
+        bool reverseflag = false;
         string commands;
-        getline(cin, commands);
+        cin >> commands;
         int listlen;
         list<int> numbers;
         cin >> listlen;
+        char junkchar;
+        cin >> junkchar;
         for (int i = 0; i < listlen; i++) {
             int input;
-            cin >> input;
+            cin >> input >> junkchar;
             numbers.push_back(input);
+        }
+        if (listlen == 0) {
+            cin >> junkchar;
+        }
+        int commandslen = commands.length();
+        for(int i = 0; i < commandslen; i++) {
+            if (commands[i] == 'R') {
+                if (reverseflag == false) {
+                    reverseflag = true;
+                } else {
+                    reverseflag = false;
+                }
+            } else if (commands[i] == 'D') {
+                if (!numbers.empty() && reverseflag == false) {
+                    numbers.pop_front();
+                } else if (!numbers.empty() && reverseflag == true) {
+                    numbers.pop_back();
+                } else {
+                    cout << "error" << endl;
+                    breakflag = true;
+                    break;
+                }
+            }
+        }
+        if (!numbers.empty() && reverseflag == false) {
+            cout << '[';
+            while(!numbers.empty() && numbers.size() != 1) {
+                cout << numbers.front() << ',';
+                numbers.pop_front();
+            }
+            cout << numbers.front();
+            cout << ']' << endl;
+        } else if (!numbers.empty() && reverseflag == true) {
+            cout << '[';
+            while(!numbers.empty() && numbers.size() != 1) {
+                cout << numbers.back() << ',';
+                numbers.pop_back();
+            }
+            cout << numbers.front();
+            cout << ']' << endl;
+        } else if (breakflag == false) {
+            cout << "[]" << endl;
         }
     }
     return 0;
