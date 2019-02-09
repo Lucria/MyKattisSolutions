11aac60ebee454f32f400c1a1649d783e3fb6514
Lucria <jerryzhang28eclipse@gmail.com>
2019-02-10 01:17:05 +0800

    [Feat] Finished PS2 Questions (CoconutSplat, FerryLoading3 and GuessTheDataStructure

---
+++ FerryLoading3.cpp
@@ -0,0 +1,16 @@
+//
+// Created by JerryZhang on 9/2/2019.
+//
+
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
---
+++ ICanGuessTheDataStructure.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <string.h>
+#include <string>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    int cases;
+    while(cin >> cases) { //Waiting for EOF
+        bool qflag = true;
+        bool stflag = true;
+        bool pqflag = true;
+        queue <int> qlist;
+        stack <int> stlist;
+        priority_queue <int> pqlist;
+        while(cases--) {
+            int command, value;
+            cin >> command >> value;
+            if (command == 1) {
+                if (qflag) {
+                    qlist.push(value);
+                }
+                if (stflag) {
+                    stlist.push(value);
+                }
+                if (pqflag) {
+                    pqlist.push(value);
+                }
+            } else {
+                if (qflag) {
+                    if (qlist.empty() || qlist.front() != value) {
+                        qflag = false;
+                    } else {
+                        qlist.pop();
+                    }
+                }
+                if (stflag) {
+                    if (stlist.empty() || stlist.top() != value) {
+                        stflag = false;
+                    } else {
+                        stlist.pop();
+                    }
+                }
+                if (pqflag) {
+                    if (pqlist.empty() || pqlist.top() != value) {
+                        pqflag = false;
+                    } else {
+                        pqlist.pop();
+                    }
+                }
+            }
+        }
+
+        if (!qflag && !stflag && !pqflag) {
+            cout << "impossible" << endl;
+        } else if (qflag && !stflag && !pqflag) {   
+            cout << "queue" << endl;
+        } else if (!qflag && stflag && !pqflag) {
+            cout << "stack" << endl;
+        } else if (!qflag && !stflag && pqflag) {
+            cout << "priority queue" << endl;
+        } else {
+            cout << "not sure" << endl;
+        }
+    }
+    return 0;
+}
