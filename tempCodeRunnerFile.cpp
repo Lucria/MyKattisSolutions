976ccd0a67070bab4f75864790a06b468745d19e
Lucria <jerryzhang28eclipse@gmail.com>
2019-02-18 09:01:23 +0800

    [Feat] Updated Solution for GameOfThrown

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <string.h>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    int students, commands;
+    stack <int> s;
+    cin >> students >> commands;
+    int sum = 0;
+    for (int i = 0; i < commands; i++) {
+        string input;
+        cin >> input;
+        if (input == "undo") {
+            int number;
+            cin >> number;
+            for (int j = 0; j < number; j++) {
+                s.pop();
+            }
+        } else {
+            s.push((input[0] - '0'));
+        }
+    }
+    while (!s.empty()) {
+        sum += s.top();
+        sum %= students;
+        s.pop();
+    }
+    cout << sum << endl;
+    return 0;
+}
