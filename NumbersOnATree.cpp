3d014d866050364fb6e2c61d1ca07865f3a5dd03
Lucria <jerryzhang28eclipse@gmail.com>
2019-02-20 14:19:44 +0800

    [Feat] Completed solution for NumbersOnATree

--- NumbersOnATree.cpp
+++ NumbersOnATree.cpp
@@ -13,7 +13,18 @@ int main() {
     int height;
     string commands;
     cin >> height >> commands;
-    priority_queue <int> tree;
-    
+    int index = 1;
+    int length = commands.length();
+    for(int i = 0; i < length; i++) {
+        if (commands[i] == 'L') {
+            index *= 2;
+        } else if (commands[i] == 'R') {
+            index = index * 2 + 1;
+        }
+    }
+    // cout << pow(2, height + 1) << endl;
+    // cout << index << endl;
+    cout << (int)pow(2, height + 1) - index << endl;
+    //cout << (1<<(height+1)) - ans << endl;
     return 0;
 }
