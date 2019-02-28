f9659bf05aef3d5c5ed0dfc60566303305d88d5c
Lucria <jerryzhang28eclipse@gmail.com>
2019-03-01 00:40:42 +0800

    [Feat] Added solution to QuickEstimates.cpp

---
+++ QuickEstimates.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <string.h>
+#include <string>
+#include <list>
+#include <stack>
+#include <set>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long ll;
+#define M_PI 3.14159265358979323846
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    int cases;
+    cin >> cases;
+    while (cases--) {
+        string cost;
+        cin >> cost;
+        cout << cost.length() << endl;
+    }
+    return 0;
+}
