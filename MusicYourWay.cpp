0073648745074f5c1220fac5016e606e588e8fe5
Lucria <jerryzhang28eclipse@gmail.com>
2019-03-18 16:56:53 +0800

    [Feat] Added solution to MusicYourWay

--- MusicYourWay.cpp
+++ MusicYourWay.cpp
@@ -18,6 +18,7 @@
 #include <queue>
 #include <map>
 #include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
@@ -25,15 +26,32 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    string attributes;
-    getline(cin, attributes);
+    string inattributes;
+    getline(cin, inattributes);
+    vector <string> attributes;
+    istringstream iss (inattributes);
+    string token;
+    while (getline(iss, token, ' ')) {
+        attributes.push_back(token);
+    }
+    // cout << " ATTRIBUTES! " << endl;
+    // for (auto i : attributes) {
+    //     cout << i << endl;
+    // }
+
     int numsongs;
     cin >> numsongs;
     cin.ignore(); //Ignore newline character
+    vector < vector <string> > allsongs;
     for (int i = 0; i < numsongs; i++) {
-        string song;
-        getline(cin, song);
-        // TODO
+        string inputsong;
+        getline(cin, inputsong);
+        istringstream iss2(inputsong);
+        vector<string> song;
+        while (getline(iss2, token, ' ')) {
+            song.push_back(token);
+        }
+        allsongs.push_back(song);
     }
     int numcommands;
     cin >> numcommands;
@@ -41,7 +59,26 @@ int main() {
     for (int j = 0; j < numcommands; j++) {
         string sortcommand;
         getline(cin, sortcommand);
-        // TODO
+        for (auto i : attributes) {
+            cout << i << " ";
+        }
+        cout << endl;
+        int counter = 0;
+        for (int k = 0; k < attributes.size(); k++) {
+            if (attributes[k] == sortcommand) {
+                counter = k;
+            }
+        }
+        stable_sort(allsongs.begin(), allsongs.end(), [counter] (vector<string> a, vector<string> b) {
+            return a[counter] < b[counter];
+        });
+        for (auto i: allsongs) {
+            for (auto j: i) {
+                cout << j << " ";
+            }
+            cout << endl;
+        }
+        
     }
     return 0;
 }
