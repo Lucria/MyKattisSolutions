15a359cbf2eea6949c8e97702e0fb4f404fd9973
Lucria <jerryzhang28eclipse@gmail.com>
2019-03-21 13:08:35 +0800

    [Feat] Added solution to MixedFractions.cpp

---
+++ MixedFractions.cpp
@@ -0,0 +1,34 @@
+// Created by Jerry
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <string.h>
+#include <string>
+#include <sstream>
+#include <list>
+#include <stack>
+#include <set>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long ll;
+#define M_PI 3.14159265358979323846
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    int up, down;
+    while (cin >> up >> down, (up || down)) {
+        
+        cout << up / down << " " << up % down << " / " << down << endl;
+    }
+    return 0;
+}
