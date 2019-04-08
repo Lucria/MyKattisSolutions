9c23f6aed77ffb75e513072f25b9b3028abe6aba
Lucria <jerryzhang28eclipse@gmail.com>
2019-04-08 22:25:51 +0800

    [Feat] Added solution to ButtonBashing.cpp

--- ButtonBashing.cpp
+++ ButtonBashing.cpp
@@ -29,11 +29,39 @@ int main() {
     while (cases--) {
         int numbuttons, desiredtime;
         cin >> numbuttons >> desiredtime;
-        vector<int> allbuttons;
-        for(int i = 0; i < numbuttons; i++) {
-            int changetime;
-            cin >> changetime;
-            allbuttons.push_back(changetime));
+        vector<int> alltimes;
+        for (int i = 0; i < numbuttons; i++) {
+            int timechange;
+            cin >> timechange;
+            alltimes.push_back(timechange);
+        }
+        vector<int> presses;
+        presses.resize(3601, INT32_MAX);
+        presses[0] = 0;
+        queue<int> bfs;
+        bfs.push(0);
+        while (!bfs.empty()) {
+            int currenttime = bfs.front();
+            bfs.pop();
+            for (auto &i : alltimes) {
+                // Iterate through alltimes, simulating pressing each button available once
+                int nexttime = currenttime + i;
+                // Remove possibility of getting negative timer
+                nexttime = max(0, nexttime);
+                // Remove possibility of exceeding maximum time limit
+                nexttime = min(3600, nexttime);
+                if (presses[nexttime] > presses[currenttime] + 1) {
+                    presses[nexttime] = presses[currenttime] + 1;
+                    bfs.push(nexttime);
+                }
+            }
+        }
+        // Will never undercook, will always be more than the desired cooking time
+        for (int i = desiredtime; i < 3601; i++) {
+            if (presses[i] != INT32_MAX) {
+                cout << presses[i] << " " << i - desiredtime << endl;
+                break;
+            }
         }
     }
     return 0;
